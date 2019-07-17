b7446552e30bcd6ed3b982b005e77529a761b430
Kamil Polanski <polanski88@gmail.com>
2019-07-17 20:32:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: "babel-loader"
+        }]
+    }
+};
