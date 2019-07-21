7ba55d13b99d47cb2741fe2c739b3c3470c2a3e3
Kamil Polanski <polanski88@gmail.com>
2019-07-21 18:57:27 +0200

    Add length of array

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = props => 
     <div>
         <h1>Aplikacja to do</h1>
-        <h4>Pozostała liczba zadań: {props.NumberOfTasks}</h4>
+        <h4>Pozostała liczba zadań: {props.NumberOfTasks.length}</h4>
     </div>
 
 export default Title;
