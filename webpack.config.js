a02b24581299008f83278b48b1c5c270557cbf78
Kamil Polanski <polanski88@gmail.com>
2019-07-21 11:19:12 +0200

    Task 20.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
+    const environment = env || 'production';
 
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
+    return {
+        mode: environment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
                         }
-                    }
-                ]
-            }
-        ]
-    }
-};
+                    ]
+                }
+            ]
+        }
+    };
+
+}
